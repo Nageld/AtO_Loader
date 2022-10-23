5611b3fc28dc7d80a64be5a4a4710ce376a6f909
Willson Fang <2542611+PhoenixICE@users.noreply.github.com>
2022-10-23 13:37:24 +1100

    Added warning about missing id field

--- Patches/CreateCardClones.cs
+++ Patches/CreateCardClones.cs
@@ -10,27 +10,22 @@ namespace AtO_Loader.Patches;
 [HarmonyPatch(typeof(Globals), "CreateCardClones")]
 public class CreateCardClones
 {
+    private const string CardsDirectoryPath = @"BepInEx\plugins\cards\";
+
     [HarmonyPrefix]
     static void SetPatch(Dictionary<string, CardData> ____CardsSource, ref string ___cardsText)
     {
-        Directory.CreateDirectory(@"BepInEx\plugins\cards\");
-        foreach (string fileName in Directory.GetFiles(@"BepInEx\plugins\cards\", "*.json", SearchOption.AllDirectories))
+        var cardDirectoryInfo = new DirectoryInfo(CardsDirectoryPath);
+        if (!cardDirectoryInfo.Exists)
+        {
+            cardDirectoryInfo.Create();
+        }
+
+        foreach (var cardFileInfo in cardDirectoryInfo.GetFiles("*.json", SearchOption.AllDirectories))
         {
             try
             {
-                var json = File.ReadAllText(fileName);
-                var newCard = ScriptableObject.CreateInstance<CardData>();
-                JsonUtility.FromJsonOverwrite(json, newCard);
-                var fileInfo = new FileInfo(fileName);
-                newCard.LoadSprite(fileName.Replace("json", "png", StringComparison.OrdinalIgnoreCase));
-                if (string.IsNullOrWhiteSpace(newCard.Id))
-                {
-                    newCard.Id = Guid.NewGuid().ToString().ToLower();
-                }
-                else
-                {
-                    newCard.Id = newCard.Id.ToLower();
-                }
+                var newCard = LoadCard(cardFileInfo);
                 if (!____CardsSource.ContainsKey(newCard.Id))
                 {
                     ___cardsText = string.Concat(new string[]
@@ -47,9 +42,29 @@ public class CreateCardClones
             }
             catch (Exception ex)
             {
-                Plugin.Logger.LogError($"{nameof(CreateCardClones)}: Failed to parse card data from json '{fileName}'");
+                Plugin.Logger.LogError($"{nameof(CreateCardClones)}: Failed to parse card data from json '{cardFileInfo.FullName}'");
                 Plugin.Logger.LogError(ex);
             }
         }
+    }
+
+    private static CardData LoadCard(FileInfo cardFileInfo)
+    {
+        var json = File.ReadAllText(cardFileInfo.FullName);
+        var newCard = ScriptableObject.CreateInstance<CardData>();
+        JsonUtility.FromJsonOverwrite(json, newCard);
+        newCard.LoadSprite(cardFileInfo.Name.Replace("json", "png", StringComparison.OrdinalIgnoreCase));
+        if (string.IsNullOrWhiteSpace(newCard.Id))
+        {
+            newCard.Id = Guid.NewGuid().ToString().ToLower();
+            Plugin.Logger.LogWarning($"Card: '{newCard.CardName}' is missing the required field 'id'. Path: {cardFileInfo.FullName}");
+            newCard.CardName = "Missing ID";
+        }
+        else
+        {
+            newCard.Id = newCard.Id.ToLower();
+        }
+
+        return newCard;
     }
 }
