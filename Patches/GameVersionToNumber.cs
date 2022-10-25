609a9190b9b765b0a97b7483fce8ac9051670614
Dylan Nagel <nageld@bxscience.edu>
2022-10-24 21:12:09 -0400

    fix issue when viewing saves

---
+++ Patches/GameVersionToNumber.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+
+namespace AtO_Loader.Patches;
+
+[HarmonyPatch(typeof(Functions), "GameVersionToNumber")]
+public class GameVersionToNumber
+{
+    [HarmonyPrefix]
+    static void SetPatch(ref string _str)
+    {
+        if (_str.Substring(0, 1).Equals("m"))
+        {
+            _str = _str.Remove(0, 1);
+        }
+    }
+}
--- Plugin.cs
+++ Plugin.cs
@@ -25,5 +25,6 @@ public partial class Plugin : BaseUnityPlugin
         harmony.PatchAll(typeof(SetWeeklyScore));
         harmony.PatchAll(typeof(SetObeliskScore));
         harmony.PatchAll(typeof(Start));
+        harmony.PatchAll(typeof(GameVersionToNumber));
     }
 }
