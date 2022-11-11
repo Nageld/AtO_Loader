ad595b57789f6c72fa1f8db3daff7ab69478348a
Nageld <Nageld@bxscience.edu>
2022-11-10 19:53:54 -0500

    check to see if there is an umodded save file (#11)

--- Patches/LoadPlayerData.cs
+++ Patches/LoadPlayerData.cs
@@ -5,10 +5,35 @@ namespace AtO_Loader.Patches;
 [HarmonyPatch(typeof(SaveManager), "LoadPlayerData")]
 public class LoadPlayerData
 {
+    private static bool unmoddedSaveCheck = false;
+
     [HarmonyPrefix]
     public static void SetPatch(ref string ___saveGameExtension, ref string ___saveGameExtensionBK)
     {
+        if (!unmoddedSaveCheck)
+        {
+            ___saveGameExtension = ".mato";
+            ___saveGameExtensionBK = ".mbak";
+        }
+    }
+
+    /// <summary>
+    /// If there is no modded save we load the original save
+    /// </summary>
+    [HarmonyPostfix]
+    public static void SetPatch(ref string ___saveGameExtension, ref string ___saveGameExtensionBK, ref PlayerData __result)
+    {
+        if (__result == null && !___saveGameExtension.Equals(".ato"))
+        {
+            ___saveGameExtension = ".ato";
+            ___saveGameExtensionBK = ".bak";
+            unmoddedSaveCheck = true;
+            __result = SaveManager.LoadPlayerData(false);
+        }
+
         ___saveGameExtension = ".mato";
         ___saveGameExtensionBK = ".mbak";
+        unmoddedSaveCheck = false;
+
     }
 }
